988e3a2d75c7bec6c77083a70b100bf91f1481d1
bsgursta <bryansgonzalez156@gmail.com>
2026-04-09 01:29:50 -0400

    get config open

--- mcu/my_app/src/threadx_threads.cpp
+++ mcu/my_app/src/threadx_threads.cpp
@@ -3,6 +3,7 @@
 extern "C" {
     #include "tx_api.h"
     #include "main.h"
+#include <stm32n657xx.h>
 
     // Prototypes for the C++ functions we want to expose to the C kernel
     void tx_application_define(void *first_unused_memory);
@@ -12,6 +13,7 @@ extern "C" {
 TX_THREAD my_thread;
 
 // These must have C linkage so the kernel can call them
+// Create all functions to call
 extern "C" void tx_application_define(void *first_unused_memory)
 {
     tx_thread_create(&my_thread, (char*)"My Thread",
@@ -23,6 +25,7 @@ extern "C" void my_thread_entry(ULONG thread_input)
 {
     while(1)
     {   
-        tx_thread_sleep(1);
+        HAL_GPIO_TogglePin(GPIOG,GPIO_PIN_8);
+        tx_thread_sleep(1000);
     }
 }
