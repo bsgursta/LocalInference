bd8f6cf8e0fc93b6e992199efe9c694f0417c53a
prototypeProto <anunfoundx@gmail.com>
2026-05-18 21:19:18 -0400

    init base maven java spring boot web service

---
+++ backend/localinference/src/test/java/com/proto/localinference/LocalinferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.localinference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalinferenceApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assert (1 == 1);
+  }
+}
