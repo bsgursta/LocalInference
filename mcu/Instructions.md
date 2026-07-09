6cf917a433eda0ee31efe7bf1910c4bac63b3adf
Bryan <bryansgonzalez156@gmail.com>
2026-07-08 22:42:28 -0400

    added bare minimum functionality for LAN functionality

---
+++ mcu/Instructions.md
@@ -0,0 +1,11 @@
+### Build Instructions Code:
+    1) Be inside the mcu folder
+    2) CTRL + SHIFT + P -> CMAKE: delete cache and reconfig
+    3) Press F7
+    4) Start Debug Session
+
+### Local TCP Setup
+    - Make sure to have an echo software for the TCP Server
+    - I use the WIFI (wlan) ip addresses instead of the eth addresses even though both devices are connected to the AP
+
+
--- mcu/Nx_TCP_Echo_Client/NetXDuo/App/app_netxduo.h
+++ mcu/Nx_TCP_Echo_Client/NetXDuo/App/app_netxduo.h
@@ -89,9 +89,9 @@ UINT MX_NetXDuo_Init(VOID *memory_ptr);
 
 #define NULL_ADDRESS             0
 
-#define DEFAULT_PORT             9000
+#define DEFAULT_PORT             5000
 #define TCP_SERVER_PORT          DEFAULT_PORT
-#define TCP_SERVER_ADDRESS       IP_ADDRESS(159, 223, 170, 57)
+#define TCP_SERVER_ADDRESS       IP_ADDRESS(192,168,1,241) //192.168.1.246
 
 #define MAX_PACKET_COUNT         100
 #define DEFAULT_MESSAGE          "dd1d2e0c-504e-445c-8e0e-136e392ef4f3\nREGISTER\n"
